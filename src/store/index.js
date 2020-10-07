773acd41b9fb9d19a23cd918184f38e703872b97
Carmarí Barreto <carmaribarreto3@gmail.com>
2020-10-07 10:42:08 -0300

    all

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,27 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    birds: []
   },
   mutations: {
+    BIRDS_GET(state, birds){state.birds = birds}
   },
   actions: {
+    BirdsGet({commit}){
+      axios.get (`http://aves.ninjas.cl/api/birds`,{
+        headers:{
+          "Content-type": "text/plain"
+        }
+      }).then((available) =>{
+      let data = available.data;
+     commit('BIRDS_GET', data)
+    }). catch(negado => console.log(negado))
+   }
   },
   modules: {
   }
